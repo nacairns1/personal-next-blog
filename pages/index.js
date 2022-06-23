901c5a103b344bd28c6fce48df1667d9229e82d8
Noah Cairns <noah.cairns1@gmail.com>
2022-06-23 11:06:20 -0400

    bug: netlify broken images

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import Hero from "../components/Hero";
 import Showcase from "../components/Showcase";
-import Resume from "../components/ResumeModal";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
 	return (
 		<div className="w-full h-full">
 			<Hero />
-			<Showcase />
-			<Resume />
+			{/* <Showcase /> */}
+
 		</div>
 	);
 }
